4658c3eff97a0634860483df020b9cd170217fe0
Jan Reyben Del Mundo <githubuserreyben@gmail.com>
2026-08-28 17:29:23 +0800

    feat: store student records and profile pictures

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -28,28 +28,36 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-    $request->validate([
-        'student_id' => 'required|unique:students,student_id',
-
-        'first_name' => 'required|string|max:100',
-        'middle_name' => 'nullable|string|max:100',
-        'last_name' => 'required|string|max:100',
-
-        'email' => 'required|email|unique:students,email',
-        'mobile_number' => 'required|numeric',
-
-        'date_of_birth' => 'required|date',
-        'gender' => 'required',
-
-        'program' => 'required',
-        'year_level' => 'required|integer',
-
-        'address' => 'required',
-
-        'profile_picture' => 'required|image|mimes:jpg,jpeg,png|max:2048',
-    ]);
-
-    return back();
+        $validated = $request->validate([
+            'student_id' => 'required|unique:students,student_id',
+    
+            'first_name' => 'required|string|max:100',
+            'middle_name' => 'nullable|string|max:100',
+            'last_name' => 'required|string|max:100',
+    
+            'email' => 'required|email|unique:students,email',
+            'mobile_number' => 'required|numeric',
+    
+            'date_of_birth' => 'required|date',
+            'gender' => 'required',
+    
+            'program' => 'required',
+            'year_level' => 'required|integer|min:1|max:6',
+    
+            'address' => 'required',
+    
+            'profile_picture' => 'required|image|mimes:jpg,jpeg,png|max:2048',
+        ]);
+    
+        $profilePicturePath = $request
+            ->file('profile_picture')
+            ->store('profile-pictures', 'public');
+    
+        $validated['profile_picture'] = $profilePicturePath;
+    
+        $student = Student::create($validated);
+    
+        return redirect()->route('students.show', $student);
     }
 
     /**
