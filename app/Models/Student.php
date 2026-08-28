d02c09cafe0fa260a0b0cd4dafaaa214f71cc89b
Jan Reyben Del Mundo <githubuserreyben@gmail.com>
2026-08-28 16:32:13 +0800

    feat: create student migration and model

---
+++ app/Models/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'student_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+        'mobile_number',
+        'date_of_birth',
+        'gender',
+        'program',
+        'year_level',
+        'address',
+        'profile_picture',
+    ];
+}
